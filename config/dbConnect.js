265c3bb36fd7370fb31ef5ec80ee997fd398428f
Akansh <akanshbende29@gmail.com>
2023-07-13 22:44:47 +0530

    db conn err1

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,9 @@
 const { default: mongoose } = require("mongoose");
 mongoose.set("strictQuery", false);
+const source = process.env.MONGODB_URL;
 const dbConnect = () => {
   try {
-    const conn = mongoose.connect(process.env.MONGODB_URL, {
+    const conn = mongoose.connect(source, {
       useNewUrlParser: true,
     });
     console.log("Database Connected Successfully");
