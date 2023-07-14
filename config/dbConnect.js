fca7158f1412e9d622616577398320aa517ddd4d
Akansh <akanshbende29@gmail.com>
2023-07-14 10:07:30 +0530

    db uri error

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,11 @@
 const { default: mongoose } = require("mongoose");
 mongoose.set("strictQuery", false);
-const source =
-  "mongodb+srv://brimwDQtRLM2tFV0:asbende@29@clusterakansh.734wjwt.mongodb.net/ShoplyDB";
-console.log(source);
+// const source =
+//   "mongodb+srv://brimwDQtRLM2tFV0:asbende@29@clusterakansh.734wjwt.mongodb.net/ShoplyDB";
+// console.log(source);
 const dbConnect = () => {
   try {
-    const conn = mongoose.connect(source);
+    const conn = mongoose.connect(process.env.MONGODB_URL);
     console.log("Database Connected Successfully");
   } catch (error) {
     console.log("DAtabase error");
