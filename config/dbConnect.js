ce7cff5209162cec264a99e29bf3338f01b76123
Akansh <akanshbende29@gmail.com>
2023-07-14 12:36:52 +0530

    uri err2

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 mongoose.set("strictQuery", false);
 const source =
-  "mongodb+srv://asbende29:HgkagbcVdF4utUyn@clusterakansh.734wjwt.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://asbende29:AkanshBende@clusterakansh.734wjwt.mongodb.net/?retryWrites=true&w=majority";
 // console.log(source);
 const dbConnect = () => {
   try {
